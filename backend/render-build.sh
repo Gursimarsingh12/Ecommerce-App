5fd4ab1e310f52353e32df13bb903784cb6be637
Gursimarsingh12 <141723338+Gursimarsingh12@users.noreply.github.com>
2024-05-19 14:30:33 +0530

    initial commit

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -8,9 +8,9 @@ STORAGE_DIR=/opt/render/project/.render
 echo "...Downloading Chrome"
 mkdir -p $STORAGE_DIR/chrome
 cd $STORAGE_DIR/chrome
-wget -P ./ https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_114.0.5735.90-1_amd64.deb
-dpkg -x ./google-chrome-stable_114.0.5735.90-1_amd64.deb $STORAGE_DIR/chrome
-rm ./google-chrome-stable_114.0.5735.90-1_amd64.deb
+wget -P ./ https://www.slimjet.com/chrome/download-chrome.php?file=files%2F103.0.5060.53%2Fgoogle-chrome-stable_current_amd64.deb
+dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+rm ./google-chrome-stable_current_amd64.deb
 cd $HOME/project/src # Make sure we return to where we were
 
 # be sure to add Chromes location to the PATH as part of your Start Command
