fa02e9d085e7d297b5da0e2f4f6b3cac29db3e29
Gursimarsingh12 <141723338+Gursimarsingh12@users.noreply.github.com>
2024-05-19 19:09:23 +0530

    Revert "initial commit"
    This reverts commit b293e6ffafece478dfc8b2a9dc6ddd69abf1fd57.

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -4,17 +4,14 @@ set -o errexit
 
 STORAGE_DIR=/opt/render/project/.render
 
-if [[ ! -d $STORAGE_DIR/chrome ]]; then
-    echo "...Downloading Chrome"
-    mkdir -p $STORAGE_DIR/chrome
-    cd $STORAGE_DIR/chrome
-    wget -P ./ https://www.slimjet.com/chrome/download-chrome.php?file=files%2F103.0.5060.53%2Fgoogle-chrome-stable_current_amd64.deb
-    dpkg -x ./download-chrome.php?file=files%2F103.0.5060.53%2Fgoogle-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
-    rm ./download-chrome.php?file=files%2F103.0.5060.53%2Fgoogle-chrome-stable_current_amd64.deb
-    cd $HOME/project/src # Make sure we return to where we were
-else
-  echo "...Using Chrome from cache"
-fi
+
+echo "...Downloading Chrome"
+mkdir -p $STORAGE_DIR/chrome
+cd $STORAGE_DIR/chrome
+wget -P ./ https://www.slimjet.com/chrome/download-chrome.php?file=files%2F103.0.5060.53%2Fgoogle-chrome-stable_current_amd64.deb
+dpkg -x ./download-chrome.php?file=files%2F103.0.5060.53%2Fgoogle-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+rm ./download-chrome.php?file=files%2F103.0.5060.53%2Fgoogle-chrome-stable_current_amd64.deb
+cd $HOME/project/src # Make sure we return to where we were
 
 # be sure to add Chromes location to the PATH as part of your Start Command
 # export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
