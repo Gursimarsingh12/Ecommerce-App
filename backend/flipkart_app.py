aae7ecb9c1adb42f348306758e13e979cf28c2cd
Gursimarsingh12 <141723338+Gursimarsingh12@users.noreply.github.com>
2024-05-19 19:14:12 +0530

    initial commit

--- backend/flipkart_app.py
+++ backend/flipkart_app.py
@@ -7,8 +7,6 @@ from selenium.webdriver.common.by import By
 
 app = FastAPI()
 
-
-
 custom_headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
